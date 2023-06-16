414594894189d68f1e1b18f4a955c8a85ce77db3
Divyanshu <96394150+verma-divyanshu-git@users.noreply.github.com>
2023-06-16 16:04:14 +0530

    Add files via upload

---
+++ Trees/Binary Tree Node class structure.cpp
@@ -0,0 +1,19 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+    
+***********************************************************/
